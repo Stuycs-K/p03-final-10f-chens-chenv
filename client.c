29190e6a90b4308c56256da79c06b8996c7bc7d6
Vincent Chen <chen.vincent.ny@gmail.com>
2026-01-11 10:49:08 -0500

    removed extraneous comments, fixed indenting

--- client.c
+++ client.c
@@ -4,66 +4,45 @@
   char pieceType; //X, or O,
   int server_socket;
 
-/*
-
-$ Enter username:
-Xxxxxx
-Thank you. Entering pool…
-Entered pool. Waiting for a match… 
-Entering match with abcde…
-
-*/
-
-/*
-Opponent has placed their ejffjbge. Where do you want to place your ewhbwbgb? [enter number from 1 to 9]
-2
-Invalid! Where do you want to place your ewhbwbgb?
-3
-*/
 static void sighandler(int signo) {
     if ( signo == SIGINT ) {
-      //send smth to server so it knows this clinet has disconnecrted.
-      //close(listen_socket);
       exit(0);
     }
   }
 
   void printtheboard() {
-  printf("  %c  |  %c  |  %c  \n", board[1], board[2], board[3]); //complete
-    printf("=====*=====*=====\n");
-      printf("  %c  |  %c  |  %c  \n",board[4],board[5],board[6]); //complete
-printf("=====*=====*=====\n");
-        printf("  %c  |  %c  |  %c  \n",board[7],board[8], board[9]); //complete
+  printf("  %c  |  %c  |  %c  \n", board[1], board[2], board[3]); 
+  printf("=====*=====*=====\n");
+  printf("  %c  |  %c  |  %c  \n",board[4],board[5],board[6]); 
+  printf("=====*=====*=====\n");
+  printf("  %c  |  %c  |  %c  \n",board[7],board[8], board[9]);
 
 }
 
 void prompt() {
   char whichSpot[256];
   int spot;
-  while(1) {//keeps prompting until right
-            printf("Where do you want to place your %c, %s?\n", pieceType, buffer);
-            printf("[enter number from 1-9, numbered clockwise starting from top left]\n");
-        if(!fgets(whichSpot,255,stdin)) {
-          continue;
-        }
-        int k = sscanf(whichSpot,"%d",&spot);
-        if(k != 1) {
-          printf("Invalid! Please enter a number.\n");
-          continue;
-        }
-        if(spot < 1 || spot > 9) {
-          printf("Invalid! number should be from 1-9");
-          continue;
-        //   printf("Where do you want to place your %s, %s?\n", pieceType, buffer);
-        //   printf("[enter number from 1-9, numbered clockwise starting from top left]\n");
-        // 
-        if (board[spot] != ' ') {
+  while(1) {
+    printf("Where do you want to place your %c, %s?\n", pieceType, buffer);
+    printf("[enter number from 1-9, numbered clockwise starting from top left]\n");
+    if(!fgets(whichSpot,255,stdin)) {
+      continue;
+    }
+    int k = sscanf(whichSpot,"%d",&spot);
+    if(k != 1) {
+      printf("Invalid! Please enter a number.\n");
+      continue;
+    }
+    if(spot < 1 || spot > 9) {
+      printf("Invalid! number should be from 1-9");
+      continue;
+    if (board[spot] != ' ') {
       printf("Invalid! Spot already taken.\n");
       continue;
     }
 
-        break; //worked
-        }//also check other cases, eg taken, and adjust for wrong int second time
+    break;
+    }
   }
   board[spot] = pieceType;
   char msg[64];
@@ -117,7 +96,6 @@ void clientLogic(int server_socket){
   }
   buffer[k] = '\0';
   readinput(buffer);
-
 }
 }
 
